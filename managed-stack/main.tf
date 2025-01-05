c7872cf80921e466e89e248826fb14a1da6ddeae
Winston Yee <winsyee@gmail.com>
2025-01-04 16:45:50 -0800

    feat: See all output

--- managed-stack/main.tf
+++ managed-stack/main.tf
@@ -4,3 +4,17 @@ resource "random_password" "secret" {
   length  = 8
   special = true
 }
+
+# Output entire spacelift object
+output "spacelift" {
+  value = spacelift
+}
+
+# Alternative: output specific parts of spacelift object
+output "spacelift_stack" {
+  value = spacelift.stack
+}
+
+output "spacelift_run" {
+  value = spacelift.run
+}
